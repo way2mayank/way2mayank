6cf6406e5eae2cd7de1c17a37e773f74a9ae4c80
Mayank Srivastava <way2mayankshri@gmail.com>
2023-02-06 14:17:49 +0530

    .

--- src/controller/cardController.js
+++ src/controller/cardController.js
@@ -1,1 +1,12 @@
-const cardModel = 
+const cardModel = require("../model/cardModel");
+
+const createCard = async (req, res) => {
+  const data = req.body;
+  const { cardNumber, cardType, customerName, status, vision, customerID } =
+    data;
+  const card = await cardModel.create(data);
+  res.status(201).send({ status: true, data: card });
+};
+
+
+module.exports ={ createCard}
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
 const { customerRegister } = require("../controller/customerController");
+const { createCard } = require("../controller/cardController")
 // CUSTOMER CONTROLLER
 
 router.post("/register", customerRegister);
+
+//  CARD ROUTER
+
+router.post("/card", createCard)
 
 module.exports = router;
